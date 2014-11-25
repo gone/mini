2aa0cd925b8dec954f2270434e96fd99bb323ebc
David Kerkeslager <kerkeslager@gmail.com>
2014-11-24 23:46:28 -0500

    Some spacing and renaming cleanup

--- sexp.py
+++ sexp.py
@@ -1,8 +1,6 @@
 import re
  
-dbg = False
-  
-term_regex = r'''(?mx)
+token_regex = r'''(?mx)
     \s*(?:
     (?P<brackl>\()|
     (?P<brackr>\))|
@@ -11,39 +9,35 @@ term_regex = r'''(?mx)
     (?P<s>[^(^)\s]+)
     )'''
                                                       
-def parse_sexp(sexp):
+def parse(source):
     stack = []
     out = []
-    if dbg: print("%-6s %-14s %-44s %-s" % tuple("term value out stack".split()))
-    for termtypes in re.finditer(term_regex, sexp):
+
+    for termtypes in re.finditer(token_regex, source):
         term, value = [(t,v) for t,v in termtypes.groupdict().items() if v][0]
-        if dbg: print("%-7s %-14s %-44r %-r" % (term, value, out, stack))
-        if   term == 'brackl':
+
+        if term == 'brackl':
             stack.append(out)
             out = []
+
         elif term == 'brackr':
             assert stack, "Trouble with nesting of brackets"
             tmpout, out = out, stack.pop(-1)
             out.append(tmpout)
+
         elif term == 'num':
             v = float(value)
             if v.is_integer(): v = int(v)
             out.append(v)
+
         elif term == 'sq':
             out.append(value[1:-1])
+
         elif term == 's':
             out.append(value)
+
         else:
             raise NotImplementedError("Error: %r" % (term, value))
+
     assert not stack, "Trouble with nesting of brackets"
     return out[0]
-                                                                                                             
-def print_sexp(exp):
-    out = ''
-    if type(exp) == type([]):
-        out += '(' + ' '.join(print_sexp(x) for x in exp) + ')'
-    elif type(exp) == type('') and re.search(r'[\s()]', exp):
-        out += '"%s"' % repr(exp)[1:-1].replace('"', '\"''"')
-    else:
-        out += '%s' % exp
-    return out
