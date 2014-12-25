1c16bfb6a0e9f3bfeabb011bfea13a806e9e636a
David Kerkeslager <kerkeslager@gmail.com>
2014-12-24 19:50:19 -0500

    Added a function for command line arguments

--- minimalisp.py
+++ minimalisp.py
@@ -485,6 +485,7 @@ if __name__ == '__main__':
 
         environment = nest(dict(predefineds))
         environment['__file__'] = os.path.join(os.path.realpath(filename))
+        environment['__arguments__'] = tuple(arguments)
         
         with open(filename,'r') as f:
             source = f.read()
